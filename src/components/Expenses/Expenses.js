42fdf4fed2cb2bac362aa6c0122b19a9ed9609f0
Juho Kangas <juhokn@gmail.com>
2021-05-04 15:54:12 +0300

    Add filter function

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,22 @@
+import React, { useState } from "react";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 
 export default function Expenses(props) {
+  const [expensesFilter, setExpensesFilter] = useState("2021");
   const list = props.list;
-  const expensesList = list.map((item) => {
+
+  const filterSelectHandler = (year) => {
+    setExpensesFilter(year);
+  };
+
+  const filteredList = list.filter((expense) => {
+    return expense.date.getFullYear().toString() === expensesFilter;
+  });
+
+  const expensesList = filteredList.map((item) => {
     return (
       <ExpenseItem
         key={item.id}
@@ -15,5 +27,13 @@ export default function Expenses(props) {
     );
   });
 
-  return <Card className="expenses">{expensesList}</Card>;
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        onFilterSelect={filterSelectHandler}
+        selected={expensesFilter}
+      />
+      {expensesList}
+    </Card>
+  );
 }
