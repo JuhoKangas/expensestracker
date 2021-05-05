16210bb71ac45da569f19177fdaa01de51dd2ee1
Juho Kangas <juhokn@gmail.com>
2021-05-05 14:09:38 +0300

    Fix the charts to be filled by percentage of total

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,7 +4,7 @@ import "./Chart.css";
 
 const Chart = (props) => {
   const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
-  const maxValue = Math.max(...dataPointValues);
+  const totalValue = dataPointValues.reduce((a, b) => a + b);
 
   return (
     <div className="chart">
@@ -12,7 +12,7 @@ const Chart = (props) => {
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={maxValue}
+          totalValue={totalValue}
           label={dataPoint.label}
         />
       ))}
